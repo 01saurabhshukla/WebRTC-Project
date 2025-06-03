04b46b0231e742a78b2d2a0456eae8c632455334
Saurabh <103saurabhshukla@gmail.com>
2025-06-03 19:06:25 +0530

    inital commit

---
+++ public/index.js
@@ -0,0 +1,48 @@
+const io = require('socket.io-client');
+const mediasoupClient = require('mediasoup-client');
+
+const socket = io("/mediasoup");
+
+socket.on('connection-success', ({ socketId }) => {
+    console.log(socketId);
+});
+
+let params = {
+  // 
+}
+
+const streamSuccess = async (stream) => {
+  localVideo.srcObject = stream
+  const track = stream.getVideoTracks()[0]
+  params = {
+    track,
+    ...params
+  }
+}
+
+const getLocalStream = () => {
+  navigator.getUserMedia({
+    audio: false,
+    video: {
+      width: {
+        min: 640,
+        max: 1020
+      },
+      height: {
+        min: 400,
+        max: 1080,
+      }
+    }
+  }, streamSuccess, error => {
+    console.log(error.message);
+  });
+}
+
+
+btnLocalVideo.addEventListener('click', getLocalStream)
+btnRtpCapabilities.addEventListener('click', getRtpCapabilities)
+// btnDevice.addEventListener('click', createDevice)
+// btnCreateSendTransport.addEventListener('click', createSendTransport)
+// btnConnectSendTransport.addEventListener('click', connectSendTransport)
+// btnRecvSendTransport.addEventListener('click', createRecvTransport)
+// btnConnectRecvTransport.addEventListener('click', connectRecvTransport)
